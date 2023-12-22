92ba57376221eca99fd0560fcda5e0b17b349826
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-12-22 10:59:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
+	let arr =[];
+	for(let i =0;i<l;i++)
+		{
+			let a = Math.floor(Math.random()*26)+65;
+			arr.push(String.fromCharCode(a));
+			
+		}
+	return arr.join('');
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l)); 
